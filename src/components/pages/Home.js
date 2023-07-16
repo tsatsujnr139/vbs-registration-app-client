82804c2b7e02578dcd543a5aa57ef48b3928e2bd
Tsatsu Adogla-Bessa Jnr <tsatsujnr@gmail.com>
2023-07-16 22:46:46 +0000

    add participant registration end date

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,10 @@
-import { Button, Col, Row, Typography } from 'antd'
-import { Layout, Spin } from 'antd'
-import React, { Fragment, useEffect, useState } from 'react'
+import { Button, Col, Row, Typography } from "antd"
+import { Layout, Spin } from "antd"
+import React, { Fragment, useEffect, useState } from "react"
 
-import Footer from '../layouts/Footer'
-import Logo from '../../static/images/logo-main.png'
-import Navbar from '../layouts/HomeNavbar'
+import Footer from "../layouts/Footer"
+import Logo from "../../static/images/logo-main.png"
+import Navbar from "../layouts/HomeNavbar"
 
 const Home = () => {
   const { Content } = Layout
@@ -17,20 +17,22 @@ const Home = () => {
     // eslint-disable-next-line
   }, [])
 
+  const registrationEndDate = new Date(2023, 6, 16, 23, 59, 59)
+
   if (loading) {
     return (
       <Spin
-        size='large'
-        style={{ display: 'block', marginTop: '100px' }}
+        size="large"
+        style={{ display: "block", marginTop: "100px" }}
       />
     )
   } else {
     return (
       <Fragment>
         <Content>
-          <div className='landing-page'>
+          <div className="landing-page">
             <Navbar />
-            <div className='homepage-hero'>
+            <div className="homepage-hero">
               <Row>
                 <Col
                   span={10}
@@ -40,10 +42,10 @@ const Home = () => {
                   sm={24}
                   xs={24}
                 >
-                  <div className='hero-logo'>
+                  <div className="hero-logo">
                     <img
                       src={Logo}
-                      alt='logo'
+                      alt="logo"
                     />
                   </div>
                 </Col>
@@ -56,23 +58,23 @@ const Home = () => {
                   xs={24}
                 >
                   <div
-                    className='register-invite'
+                    className="register-invite"
                     style={{
-                      paddingBottom: '20px'
+                      paddingBottom: "20px"
                     }}
                   >
-                    <div className='hero-motto'>
+                    <div className="hero-motto">
                       <Title level={2}>
                         Vacation Bible School {new Date().getFullYear()}
                       </Title>
                     </div>
-                    <div className='hero-tagline'>
+                    <div className="hero-tagline">
                       <Title level={4}></Title>
                     </div>
-                    <div className='hero-motto'>
+                    <div className="hero-motto">
                       <Title>Following Jesus Changes the Game.</Title>
                     </div>
-                    <div className='hero-tagline'>
+                    <div className="hero-tagline">
                       <Title level={4}>
                         Spin the spinner, beat the clock, skip ahead, level up,
                         and play to win! You’ll need to bring your A-game for
@@ -84,29 +86,47 @@ const Home = () => {
                         when they mess up it’s never “game over.”
                       </Title>
                     </div>
-                    <Button
-                      size='large'
-                      shape='round'
-                      type='primary'
-                      href='participants/register'
-                    >
-                      REGISTER PARTICIPANT
-                    </Button>
-                    <span
-                      style={{
-                        color: 'white',
-                        paddingTop: '10px',
-                        paddingBottom: '10px'
-                      }}
-                    >
-                      {' '}
-                      OR{' '}
-                    </span>
+                    {/* Check if date is greater than 16th July, 2023 */}
+                    {new Date() <= registrationEndDate ? (
+                      <>
+                        <Button
+                          size="large"
+                          shape="round"
+                          type="primary"
+                          href="participants/register"
+                        >
+                          REGISTER PARTICIPANT
+                        </Button>
+                        <span
+                          style={{
+                            color: "white",
+                            paddingTop: "10px",
+                            paddingBottom: "10px"
+                          }}
+                        >
+                          {" "}
+                          OR{" "}
+                        </span>
+                      </>
+                    ) : (
+                      <p
+                        style={{
+                          color: "red",
+                          paddingTop: "10px",
+                          paddingBottom: "10px",
+                          fontSize: "20px",
+                          fontWeight: "bold",
+                          textDecoration: "underline"
+                        }}
+                      >
+                        PARTICIPANT REGISTRATION CLOSED
+                      </p>
+                    )}
 
                     <Button
-                      size='large'
-                      shape='round'
-                      href='volunteers/register'
+                      size="large"
+                      shape="round"
+                      href="volunteers/register"
                     >
                       SIGN UP AS A VOLUNTEER
                     </Button>
